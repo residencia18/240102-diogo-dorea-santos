2da966d5548ce66fbf03ba20419ff86ad4f591e8
Diogo Dorea <diogodorea@gmail.com>
2023-11-15 01:32:23 -0300

    Implementação arraylist de usuarios e mostrar postagens

--- RedeSocial/src/RedeSocial/Usuario.java
+++ RedeSocial/src/RedeSocial/Usuario.java
@@ -7,37 +7,43 @@ public class Usuario {
 	private String nome, email, nacionalidade;
 	private ArrayList<String> postagens;
 	private String postagem;
-	private int quantidadeDePostagens;
+	private int quantidadeDePostagens, pontuacao;
 	
+	//COntrutor para o passo 1 do exercicio
+	/*
 	public Usuario() {
 		postagens = new ArrayList<String>();
 		this.setQuantidadeDePostagens(0);
+		this.setPontuacao(0);
+	}
+	*/
+	
+	public Usuario(String _nomeUsuario) {
+		postagens = new ArrayList<String>();
+		this.setQuantidadeDePostagens(0);
+		this.setPontuacao(0);
+		this.setNome(_nomeUsuario);
 	}
 
 	public String getNome() {
 		return nome;
 	}
 
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getNacionalidade() {
 		return nacionalidade;
 	}
-
 
 	public void setNacionalidade(String nacionalidade) {
 		this.nacionalidade = nacionalidade;
@@ -50,10 +56,17 @@ public class Usuario {
 	public void setPostagem(String postagem) {
 		this.postagem = postagem;
 	}
-
 	
 	public int getQuantidadeDePostagens() {
 		return quantidadeDePostagens;
+	}
+	
+	public int getPontuacao() {
+		return pontuacao;
+	}
+
+	public void setPontuacao(int pontuacao) {
+		this.pontuacao = pontuacao;
 	}
 
 	public void setQuantidadeDePostagens(int quantidadeDePostagens) {
@@ -61,11 +74,18 @@ public class Usuario {
 	}
 
 	public void adicionaPostagens(String post) {
-		
+		quantidadeDePostagens++;		
 	}
-
+	public void alteraPontuacao(int delta) {
+		pontuacao += delta;
+	}
+	
+		
+	//Metodo main
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Passo 1 do exercicio
+		/*
 		Usuario usuario = new Usuario();
 		Scanner sc = new Scanner(System.in);
 		
@@ -78,8 +98,36 @@ public class Usuario {
 		System.out.println("Digite a sua postagem: ");
 		usuario.setPostagem(sc.nextLine());
 		usuario.adicionaPostagens(usuario.getPostagem());
+		*/
 		
+		//Passo 2 do exercicio
 		
+		ArrayList<Usuario> listaUsuario = new ArrayList<Usuario>();
+		Scanner sc = new Scanner(System.in);
+		
+		String sn;
+		do {
+			System.out.println("Digite o nome do usuário: ");
+			String nomeUsuario = sc.nextLine();
+			Usuario usuario = new Usuario(nomeUsuario);
+			System.out.println("Digite o email do usuario: ");
+			usuario.setEmail(sc.nextLine());
+			System.out.println("Digite o nacionalidade do usuario: ");
+			usuario.setNacionalidade(sc.nextLine());
+			System.out.println("Digite a sua postagem: ");
+			usuario.setPostagem(sc.nextLine());
+			usuario.adicionaPostagens(usuario.getPostagem());
+			listaUsuario.add(usuario);
+			System.out.println("deseja inserir mais um usuario: s ou n");
+			sn = sc.nextLine();
+			System.out.println(sn);
+		} while (!sn.equals("n"));	
+		sc.close();
+		
+		for (int i = 0; i < listaUsuario.size(); i++) {
+			System.out.println(listaUsuario.get(i).getPostagem());
+			
+		}
 		
 	}
 
