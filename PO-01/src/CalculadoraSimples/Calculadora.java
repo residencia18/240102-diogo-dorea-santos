2a4f0c645561a0f015c7d2c9ce14befb2fbb6d8f
Diogo Dorea <diogodorea@gmail.com>
2023-11-15 02:39:28 -0300

    PO-01 CAlculadora e primeiras questoes

---
+++ PO-01/src/CalculadoraSimples/Calculadora.java
@@ -0,0 +1,50 @@
+package CalculadoraSimples;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número:");
+        double numero1 = scanner.nextDouble();
+
+        System.out.println("Digite o segundo número:");
+        double numero2 = scanner.nextDouble();
+
+        System.out.println("Escolha a operação:");
+        System.out.println("1 - Adição");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Multiplicação");
+        System.out.println("4 - Divisão");
+        int escolha = scanner.nextInt();
+
+        double resultado = 0;
+
+        switch (escolha) {
+            case 1:
+                resultado = numero1 + numero2;
+                break;
+            case 2:
+                resultado = numero1 - numero2;
+                break;
+            case 3:
+                resultado = numero1 * numero2;
+                break;
+            case 4:
+                if (numero2 != 0) {
+                    resultado = numero1 / numero2;
+                } else {
+                    System.out.println("Não é possível dividir por zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Escolha inválida!");
+                return;
+        }
+
+        System.out.println("O resultado da operação é: " + resultado);
+        scanner.close();
+    }
+}
