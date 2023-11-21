77fcbd18876edae35ea6be2248ee3a11e0cbdc59
Diogo Dorea <diogodorea@gmail.com>
2023-11-21 10:05:31 -0300

    Ajuste P001 fechamento do scanner em calculadora Java

--- PO-01/src/CalculadoraSimples/Calculadora.java
+++ PO-01/src/CalculadoraSimples/Calculadora.java
@@ -8,7 +8,7 @@ public class Calculadora {
 
         System.out.println("Digite o primeiro número:");
         double numero1 = scanner.nextDouble();
-
+        
         System.out.println("Digite o segundo número:");
         double numero2 = scanner.nextDouble();
 
@@ -36,11 +36,14 @@ public class Calculadora {
                     resultado = numero1 / numero2;
                 } else {
                     System.out.println("Não é possível dividir por zero.");
+                    scanner.close();
                     return;
                 }
                 break;
             default:
                 System.out.println("Escolha inválida!");
+              
+                scanner.close();
                 return;
         }
 
