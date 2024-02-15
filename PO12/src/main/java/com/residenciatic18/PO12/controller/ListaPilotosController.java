c4a55de771b097b1c8da01879e5ecb7316a0b887
Diogo Dorea <diogodorea@gmail.com>
2024-02-15 02:05:36 -0300

    PO12 lista top5 e top10 vencedores, vitorias por pais

--- PO12/src/main/java/com/residenciatic18/PO12/controller/ListaPilotosController.java
+++ PO12/src/main/java/com/residenciatic18/PO12/controller/ListaPilotosController.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -72,33 +73,66 @@ public class ListaPilotosController {
 	
 	
 	@RequestMapping("/top5")
-	public ArrayList<Piloto> getPilotosTop5() {
+	public ArrayList<Piloto> getTop5Pilotos() {
 		
-		try {
-			String nomeArquivo = "pilotos.csv";
-			FileReader fr = new FileReader(nomeArquivo);
-			BufferedReader br = new BufferedReader(fr);	
-			
-			ArrayList<Piloto> pilotos = new ArrayList<>();
-			String linha = br.readLine();
-			while(linha != null) {
+		
 				
-				String[] dados = linha.split(";");
-				Piloto p = new Piloto(dados[0], dados[1], Integer.parseInt(dados[2]));
-				if (p.getVitorias() >= 5) {
-					pilotos.add(p);
-				}
-				linha = br.readLine();
+			
+			ArrayList<Piloto> pilotos = ListaPilotosController.getPilotos();
+			ArrayList<Piloto> top5 = new ArrayList<>();
+			pilotos.sort(Comparator.comparingInt(Piloto::getVitorias).reversed());
+			for (int i = 0; i < 5; i++) {
+				top5.add(pilotos.get(i));
 			}
-			br.close();
-			fr.close();
-			return pilotos;
-		} catch (IOException e) {
-			e.printStackTrace();
+				
+			return top5;
+			
+			
 		}
-		//System.out.println(pilotos.toString());
-		return null;
-	}
+	
+	@RequestMapping("/top10")
+	public ArrayList<Piloto> getTop10Pilotos() {
+		
+		
+				
+			
+			ArrayList<Piloto> pilotos = ListaPilotosController.getPilotos();
+			ArrayList<Piloto> top10 = new ArrayList<>();
+			pilotos.sort(Comparator.comparingInt(Piloto::getVitorias).reversed());
+			for (int i = 0; i < 10; i++) {
+				top10.add(pilotos.get(i));
+			}
+				
+			return top10;
+			
+			
+		}
+	
+	@RequestMapping("/porpais")
+	public ArrayList<String> vitoriasPorPais() {
+		
+		
+				
+			
+			ArrayList<Piloto> pilotos = ListaPilotosController.getPilotos();
+			ArrayList<String> vitoriasPorPais = new ArrayList<String>();
+			pilotos.sort(Comparator.comparingInt(Piloto::getVitorias).reversed());
+			for (Piloto p : pilotos) {
+				
+				if (!vitoriasPorPais.contains(p.getPais())) {
+					vitoriasPorPais.add(p.getPais()
+							+ ": " + p.getVitorias()
+							+ " vitorias");
+				}
+				
+			}
+				
+			return vitoriasPorPais;
+			
+			
+		}
+	
+	
 		
 	
 
