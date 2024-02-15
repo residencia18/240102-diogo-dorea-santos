5da9abcbb720f8a2bb3ecf611797e280462489a0
Diogo Dorea <diogodorea@gmail.com>
2024-02-15 00:01:18 -0300

    PO12 repositorio base

---
+++ PO12/src/main/java/com/residenciatic18/PO12/Po12Application.java
@@ -0,0 +1,13 @@
+package com.residenciatic18.PO12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Po12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Po12Application.class, args);
+	}
+
+}
---
+++ PO12/src/test/java/com/residenciatic18/PO12/Po12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.residenciatic18.PO12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Po12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
