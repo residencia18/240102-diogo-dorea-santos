f0fb90b63aa70f323052cff6b260c9a382092615
Diogo Dorea <diogodorea@gmail.com>
2024-02-15 02:32:02 -0300

    PO12 CODEREVIEW

--- PO12/src/main/java/com/residenciatic18/PO12/controller/ListaPilotosController.java
+++ PO12/src/main/java/com/residenciatic18/PO12/controller/ListaPilotosController.java
@@ -16,8 +16,6 @@ import model.Piloto;
 @RestController
 public class ListaPilotosController {
 	
-	//private static ArrayList<Piloto> pilotos = ListaPilotosController.getPilotos();
-	
 	@RequestMapping("/todos")
 	public static ArrayList<Piloto> getPilotos() {
 		
@@ -29,7 +27,6 @@ public class ListaPilotosController {
 			ArrayList<Piloto> pilotos = new ArrayList<>();
 			String linha = br.readLine();
 			while(linha != null) {
-				
 				String[] dados = linha.split(";");
 				Piloto p = new Piloto(dados[0], dados[1], Integer.parseInt(dados[2]));
 				pilotos.add(p);
@@ -41,9 +38,7 @@ public class ListaPilotosController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		//System.out.println(pilotos.toString());
 		return null;
-		
 	}
 	
 	@RequestMapping("/")
@@ -54,69 +49,41 @@ public class ListaPilotosController {
 	
 	@RequestMapping("/brasileiros")
 	public ArrayList<Piloto> getPilotosBrasileiros() {
-		
-		
-				
-			
-			ArrayList<Piloto> pilotos = ListaPilotosController.getPilotos();
-			ArrayList<Piloto> brasileiros = new ArrayList<>();
-			for (Piloto p : pilotos) {
-				
-				if (p.getPais().equals("Brasil")) {
-					brasileiros.add(p);
-				}
-				
-			}
-				
-			return brasileiros;
-			
-			
+		ArrayList<Piloto> pilotos = ListaPilotosController.getPilotos();
+		ArrayList<Piloto> brasileiros = new ArrayList<>();
+		for (Piloto p : pilotos) {
+			if (p.getPais().equals("Brasil")) {
+				brasileiros.add(p);
+			}		
 		}
+		return brasileiros;
+	}
 		
-	
-	
 	@RequestMapping("/top5")
 	public ArrayList<Piloto> getTop5Pilotos() {
-		
-		
-				
-			
-			ArrayList<Piloto> pilotos = ListaPilotosController.getPilotos();
-			ArrayList<Piloto> top5 = new ArrayList<>();
-			pilotos.sort(Comparator.comparingInt(Piloto::getVitorias).reversed());
-			for (int i = 0; i < 5; i++) {
-				top5.add(pilotos.get(i));
-			}
-				
-			return top5;
-			
-			
+		ArrayList<Piloto> pilotos = ListaPilotosController.getPilotos();
+		ArrayList<Piloto> top5 = new ArrayList<>();
+		pilotos.sort(Comparator.comparingInt(Piloto::getVitorias).reversed());
+		for (int i = 0; i < 5; i++) {
+			top5.add(pilotos.get(i));
 		}
+		return top5;
+	}
 	
 	@RequestMapping("/top10")
 	public ArrayList<Piloto> getTop10Pilotos() {
-		
-		
-				
-			
-			ArrayList<Piloto> pilotos = ListaPilotosController.getPilotos();
-			ArrayList<Piloto> top10 = new ArrayList<>();
-			pilotos.sort(Comparator.comparingInt(Piloto::getVitorias).reversed());
-			for (int i = 0; i < 10; i++) {
-				top10.add(pilotos.get(i));
-			}
-				
-			return top10;
-			
-			
-		}
+		ArrayList<Piloto> pilotos = ListaPilotosController.getPilotos();
+		ArrayList<Piloto> top10 = new ArrayList<>();
+		pilotos.sort(Comparator.comparingInt(Piloto::getVitorias).reversed());
+		for (int i = 0; i < 10; i++) {
+			top10.add(pilotos.get(i));
+		}	
+		return top10;
+	}
 	
 	/*
 	@RequestMapping("/porpais")
 	public ArrayList<String> vitoriasPorPais() {
-		
-		
-				
 			
 			ArrayList<Piloto> pilotos = ListaPilotosController.getPilotos();
 			ArrayList<String> vitoriasPorPais = new ArrayList<String>();
@@ -127,14 +94,10 @@ public class ListaPilotosController {
 					vitoriasPorPais.add(p.getPais()
 							+ ": " + p.getVitorias()
 							+ " vitorias");
-				}
-				
-			}
-				
+				}	
+			}	
 			return vitoriasPorPais;
-			
-			
-		}
+	}
 	*/
 	
 	@RequestMapping("/porpais")
@@ -185,7 +148,4 @@ public class ListaPilotosController {
 
         return mediaVitoriasPorPaisOrdenada;
     }
-		
-	
-
 }
