202000e18532916f0fc6d87a600bfb8eb147f63f
Diogo Dorea <diogodorea@gmail.com>
2024-02-20 16:19:19 -0300

    redesocial GetMapping

--- redesocial/src/main/java/com/residenciatic18/redesocial/controller/UsuarioController.java
+++ redesocial/src/main/java/com/residenciatic18/redesocial/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.residenciatic18.redesocial.dto.UsuarioDTO;
@@ -11,27 +12,42 @@ import com.residenciatic18.redesocial.model.Usuario;
 import com.residenciatic18.redesocial.repository.UsuarioRepository;
 
 @RestController
+@RequestMapping("/usuarios/")
 public class UsuarioController {
 	
 	@Autowired 
 	private UsuarioRepository usuariorepository;
 	
-	@RequestMapping("/listausuarios")
-	public ArrayList<UsuarioDTO> getUsuario() {
-		
-		List<Usuario> usuarios = (ArrayList<Usuario>) usuariorepository.findAll();
+	@GetMapping
+	public ArrayList<UsuarioDTO> getUsuario(String nome) {
+		//System.out.println("Nome: " + nome);
+		ArrayList<UsuarioDTO> usuariosdto = new ArrayList<UsuarioDTO>();
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		if (nome == null) {
+			 usuarios = (ArrayList<Usuario>) usuariorepository.findAll();
+		}
+		else {
+			usuarios = (ArrayList<Usuario>) usuariorepository.findByNome(nome);	
+		}
+		for (Usuario u : usuarios) {
+			
+			usuariosdto.add(new UsuarioDTO(u));	
+		}
+		return usuariosdto;	
+	}
+	/*
+	@RequestMapping("/listausuariospornome/")
+	public ArrayList<UsuarioDTO> getUsuarioPorNome(String _nome) {
+
+		List<Usuario> usuarios = (ArrayList<Usuario>) usuariorepository.findByNome(_nome);
 		ArrayList<UsuarioDTO> usuariosdto = new ArrayList<UsuarioDTO>();
 		for (Usuario u : usuarios) {
 			
-			usuariosdto.add(new UsuarioDTO(u));
-			
+			usuariosdto.add(new UsuarioDTO(u));	
 		}
-		return usuariosdto;
-		
-		
-		
+		return usuariosdto;	
 	}
-
+	*/
 }
 
 /*
--- redesocial/src/main/java/com/residenciatic18/redesocial/repository/UsuarioRepository.java
+++ redesocial/src/main/java/com/residenciatic18/redesocial/repository/UsuarioRepository.java
@@ -1,11 +1,12 @@
 package com.residenciatic18.redesocial.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.residenciatic18.redesocial.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
-	
-
+	List<Usuario> findByNome(String nome);
 }
