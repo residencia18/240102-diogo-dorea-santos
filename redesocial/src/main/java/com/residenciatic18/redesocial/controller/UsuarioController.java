0745fae23ce74979c19af93b9d7fb017c6f74f5a
Diogo Dorea <diogodorea@gmail.com>
2024-02-22 16:47:59 -0300

    Aula 22-02-24 atualizacoes projeto rede social

--- redesocial/src/main/java/com/residenciatic18/redesocial/controller/UsuarioController.java
+++ redesocial/src/main/java/com/residenciatic18/redesocial/controller/UsuarioController.java
@@ -1,13 +1,21 @@
 package com.residenciatic18.redesocial.controller;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
 import com.residenciatic18.redesocial.dto.UsuarioDTO;
+import com.residenciatic18.redesocial.form.UsuarioForm;
 import com.residenciatic18.redesocial.model.Usuario;
 import com.residenciatic18.redesocial.repository.UsuarioRepository;
 
@@ -35,6 +43,40 @@ public class UsuarioController {
 		}
 		return usuariosdto;	
 	}
+	
+	/*
+	//@PostMapping recebe um Json controi o usuario,
+	 * salvando-o no banco e retorna um usuarioDTO.
+	@PostMapping
+	public UsuarioDTO inserir(@RequestBody UsuarioForm uf) {
+		
+		Usuario u = uf.criaUsuario();
+		usuariorepository.save(u);
+		return new UsuarioDTO(u);
+	}
+	 */
+	
+	/*
+	@PostMapping
+	public ResponseEntity<UsuarioDTO> inserir(@RequestBody UsuarioForm uf) {
+		
+		Usuario u = uf.criaUsuario();
+		usuariorepository.save(u);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+	*/
+	
+	@PostMapping
+	public ResponseEntity inserir(@RequestBody UsuarioForm uf, UriComponentsBuilder uriBuilder) {
+		
+		Usuario u = uf.criaUsuario();
+		usuariorepository.save(u);//apos o salvamento o usuario ganha o Id
+		UsuarioDTO udto = new UsuarioDTO(u);//usuario dto sem a senha
+		uriBuilder.path("/usuarios/{id}");
+		URI uri = uriBuilder.buildAndExpand(u.getId()).toUri();
+		return ResponseEntity.created(uri).body(udto);
+	}
+	
 	/*
 	@RequestMapping("/listausuariospornome/")
 	public ArrayList<UsuarioDTO> getUsuarioPorNome(String _nome) {
