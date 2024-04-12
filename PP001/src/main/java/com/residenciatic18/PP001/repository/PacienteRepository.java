e9e50e4c08a0360b34c73bc92bfa181cc16ae863
Diogo Dorea <diogodorea@gmail.com>
2024-04-12 15:21:12 -0300

    PP001 paciente repository

---
+++ PP001/src/main/java/com/residenciatic18/PP001/repository/PacienteRepository.java
@@ -0,0 +1,9 @@
+package com.residenciatic18.PP001.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.residenciatic18.PP001.model.Paciente;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long>{
+
+}
